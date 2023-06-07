d88e6285dace4d1a5ab98862324fd0420c94a0bc
yanliang <yanliang.tan@prometheus-communications.com>
2023-06-07 16:23:27 +0800

    testing new changes

--- src/App.js
+++ src/App.js
@@ -6,32 +6,51 @@ import { useDispatch, useSelector } from "react-redux";
 import UsersList from "./UsersList";
 import { deleteUser } from "./redux/slices/userDataSlice";
 import addUser from "./redux/slices/userDataSlice";
+// import "./collection.css";
 
 function App() {
-  const usersData = useSelector((state) => {
-    return state.usersData;
-  });
+  // const usersData = useSelector((state) => {
+  //   return state.usersData;
+  // });
 
-  const dispatch = useDispatch();
+  // const dispatch = useDispatch();
 
-  const handleDeleteUser = (id) => {
-    return () => {
-      dispatch(deleteUser(id));
-    };
-  };
+  // const handleDeleteUser = (id) => {
+  //   return () => {
+  //     dispatch(deleteUser(id));
+  //   };
+  // };
 
-  const handleAddUser = () => {
-    return () => {
-      dispatch(addUser);
-    };
-  };
+  // const handleAddUser = () => {
+  //   return () => {
+  //     dispatch(addUser);
+  //   };
+  // };
   return (
+    // <div>
+    //   {/* <div>
+    //     <UsersList
+    //       usersData={usersData}
+    //       handleDeleteUser={handleDeleteUser}
+    //       handleAddUser={handleAddUser}
+    //     ></UsersList>
+    //   </div> */}
+
+    // </div>
     <div>
-      <UsersList
-        usersData={usersData}
-        handleDeleteUser={handleDeleteUser}
-        handleAddUser={handleAddUser}
-      ></UsersList>
+      <aside class="sidebar sidebar-boxed sidebar-dark">
+        <ul class="nav sidenav dropbable">
+          <li>
+            <a href>Send Message</a>
+          </li>
+          <li>
+            <a href>Conversation</a>
+          </li>
+          <li>
+            <a href>Message Template</a>
+          </li>
+        </ul>
+      </aside>
     </div>
   );
 }
